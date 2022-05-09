d16c093180ccf12e2a7913c34dfb9bd0e82a3825
Tondon César <pilou.tondon@hotmail.fr>
2022-05-09 16:21:21 +0200

    maj ihm

--- src/main/java/fr/ul/miage/structurationDocuments/controller/MainController.java
+++ src/main/java/fr/ul/miage/structurationDocuments/controller/MainController.java
@@ -1,9 +1,9 @@
 package fr.ul.miage.structurationDocuments.controller;
 
 import fr.ul.miage.structurationDocuments.App;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 
 public class MainController {
 
@@ -11,6 +11,16 @@ public class MainController {
     public Tab tab_mod;
     public Tab tab_admin;
     public TabPane main_onglet;
+    public ListView listview_first;
+    public TextField input_top_country_artist;
+    public Button input_top_tracks;
+    public Button input_top_country_tracks;
+    public Button input_top_artists;
+    public Button input_top_tags;
+    public ListView listview_second;
+    public TextField input_tag;
+    public TextField input_album;
+    public TextField input_artiste;
 
     public void initialize() {
         switch (user) {
@@ -31,5 +41,26 @@ public class MainController {
      */
     public void end_connection(Event event) {
         App.changeScene("/javafx/connection.fxml");
+    }
+
+    public void generate_tag_results(ActionEvent actionEvent) {
+    }
+
+    public void generate_album_results(ActionEvent actionEvent) {
+    }
+
+    public void generate_artiste_results(ActionEvent actionEvent) {
+    }
+
+    public void generate_topTracks_results(ActionEvent actionEvent) {
+    }
+
+    public void generate_topCountryTracks_results(ActionEvent actionEvent) {
+    }
+
+    public void generate_topArtists_results(ActionEvent actionEvent) {
+    }
+
+    public void generate_topTags_results(ActionEvent actionEvent) {
     }
 }
